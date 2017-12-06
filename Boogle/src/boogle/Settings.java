05c124881e852ecd008fd3216f5cd07d0c67745c
Pierant2 <pierre-antoine.waxin@outlook.fr>
2017-12-06 17:15:09 +0100

    Classe settings fonctionnelle

--- Boogle/src/boogle/Settings.java
+++ Boogle/src/boogle/Settings.java
@@ -2,6 +2,7 @@
 
 package boogle;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -16,7 +17,6 @@ public class Settings {
     private String dicesLocation, dictionaryLocation;
     private int[] points = new int[6];
     private FileInputStream fis;
-    private FileOutputStream fos;
     private Properties propertiesLoader = null;
     private String filePath;
     
@@ -25,7 +25,6 @@ public class Settings {
     public void loadFile(String filePath) throws IOException {
         
         this.fis = new FileInputStream(filePath);
-        this.fos = new FileOutputStream(filePath);
         this.propertiesLoader = new Properties();
         this.propertiesLoader.load(fis);
         this.minSize = Integer.parseInt(propertiesLoader.getProperty("minimum-size"));
@@ -35,24 +34,16 @@ public class Settings {
         for (int i = 0; i < points.length; i++){
             this.points[i] = Integer.parseInt(tokens[i]);
         }
-        this.setProperty("test", "test");
+        this.setProperty("rules-4x4.properties", "truc", "truc");
     }
     
     public String getfilePath(){return this.filePath;}
     
-    public void setProperty(String varName, Object var) throws IOException {
-        if(this.propertiesLoader != null){
-            propertiesLoader.setProperty(varName, var.toString());
-            propertiesLoader.store(fos, null);
-        }
-    }
-    
-    public static void main (String[] args){
-        Settings set = new Settings("rules-4x4.properties");
-        try {
-            set.loadFile(set.getfilePath());
-        } catch (IOException ex) {
-            System.err.println("Une erreur est survenue lors du chargement du fichier de configuration : "+ex);
+    public void setProperty(String filePath, String varName, Object var) throws IOException {
+        if (this.propertiesLoader != null){
+            this.propertiesLoader.put(varName, var.toString());
+            FileOutputStream fos = new FileOutputStream(filePath);
+            this.propertiesLoader.store(fos, null);
         }
     }
 }
