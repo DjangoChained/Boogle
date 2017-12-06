bd6260cf1b0eb3516ab9d3ea825879856fdf0c4c
Pierant2 <pierre-antoine.waxin@outlook.fr>
2017-12-06 20:54:41 +0100

    Ajout de la classe représentant un dé et permettant de les charger.

--- Boogle/src/boogle/mots/Dice.java
+++ Boogle/src/boogle/mots/Dice.java
@@ -1,10 +1,39 @@
 
 package boogle.mots;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 /**
  *
  * @author waxinp
  */
 public class Dice {
     private char[] faces = new char[6];
+    
+    public Dice(String faces){
+        int pos = 0;
+        for (int i = 0; i <= faces.length(); i += 2){
+            this.faces[pos++] = faces.charAt(i);
+        }
+    }
+    
+    public static Dice[] loadDices(String pathToDices, int nbDices) throws IOException {
+        int pos = 0;
+        Dice[] dices = new Dice[nbDices];
+        List<String> lines = Files.readAllLines(Paths.get(pathToDices));
+        for (String line : lines){
+            dices[pos++] = new Dice(line);
+        }
+        return dices;
+    }
+    
+    public String toString(){
+        return String.valueOf(faces);
+    }
 }
